d1888cba0c811213773b31b138665a8cf1ddf1a5
Your Name <you@example.com>
2022-05-13 17:22:33 +0300

    autocompliter get city

--- src/app/city-input/city-input.component.ts
+++ src/app/city-input/city-input.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
+import {map, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-city-input',
@@ -9,28 +10,53 @@ import {FormControl} from "@angular/forms";
 export class CityInputComponent implements OnInit {
 
   control = new FormControl();
-  cities: string[] = ['city-one', 'city-two']
+  streets: string[] = ['city-one', 'city-two']
   apiKey = '46beb04043c94408454319d7f2b20142';
   result = {};
-  constructor() { }
+  filteredStreets: Observable<string[]> | undefined;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.filteredStreets = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    console.log(value, filterValue)
+    return this.getWeather(value)
+    // return ['1'];
+    // return this.streets.filter(street => this._normalizeValue(street).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
   }
 
 
-  httpGet(theUrl: string)
-  {
+  httpGet(theUrl: string) {
     let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
   }
-  getWeather($event: any) {
 
-    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${$event.target.value}&limit=20&appid=${this.apiKey}`;
+  getWeather(value: any) {
+
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=20&appid=${this.apiKey}`;
     let result = this.httpGet(url);
-    console.log(JSON.parse(result))
-    this.result = result;
+    let obj = JSON.parse(result)
+    let asdf: any[] = [];
+
+    for (const [key, value] of Object.entries(obj)) {
+      // @ts-ignore
+      asdf.push(value.name)
+    }
+    return asdf
   }
 
 }

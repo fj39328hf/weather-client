e923a0770a243e8d074028a658d8c8c06e14f12f
Your Name <you@example.com>
2022-05-14 11:35:30 +0300

    remove type, add object definitions

--- src/app/city-input/city-input.component.ts
+++ src/app/city-input/city-input.component.ts
@@ -10,7 +10,6 @@ import {filter, map, Observable, startWith} from "rxjs";
 export class CityInputComponent implements OnInit {
 
   control = new FormControl();
-  streets: string[] = []
   apiKey = '46beb04043c94408454319d7f2b20142';
   result = {};
   filteredStreets: Observable<any[]> | undefined;
@@ -35,9 +34,6 @@ export class CityInputComponent implements OnInit {
   }
 
   getWeather(lon:string, lat:string) {
-
-    console.log('work', this.control.value.replace(new RegExp(",.*"), ''))
-    // let url = `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}`;
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
     let result = this.httpGet(url);
     this.result = JSON.parse(result)
@@ -48,25 +44,17 @@ export class CityInputComponent implements OnInit {
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${this.apiKey}`;
     let result = this.httpGet(url);
     let obj = JSON.parse(result)
-    let asdf: string[] = [];
+    let cities = [];
     for (value of Object.entries(obj)) {
       let v = value[1];
-      let name = v.name;
-      let country = v.country;
-      let lat = v.lat;
-      let lon = v.lon;
-      // console.log()
-      // asdf.push(`${value.name}, ${value.country}, ${value.lat}`)
-      // @ts-ignore
-      asdf.push({
-        name: name,
-        country: country,
-        lat: lat,
-        lon: lon
+      cities.push({
+        name: v.name,
+        country: v.country,
+        lat: v.lat,
+        lon: v.lon
       })
     }
-    // console.log(asdf)
-    return asdf
+    return cities
   }
 
 }
